fd8865a0967acb3fc668b2220340e32546933b96
Bakar002 <abubakaramjad78600@gmail.com>
2023-03-05 01:07:53 +0500

    In this i have added all the routes for the proper swap to different pages

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,7 +1,28 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from "./components/Home/Home";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Pagenotfound from "./components/Pagenotfound/Pagenotfound";
+import Moviesdetail from "./components/Moviesdetail/Moviesdetail";
+import "./App.scss";
 
-export default function App() {
+function App() {
   return (
-    <div>App</div>
-  )
+    <div className="app">
+     <BrowserRouter>
+     <Header/>
+      <Routes>
+         
+            <Route path="/" element={<Home />} />
+        <Route path="/movie/:imdbID" element={<Moviesdetail/>} />
+        <Route path="*" element={<Pagenotfound/>} />
+        </Routes>
+        <Footer />
+       
+    </BrowserRouter>
+    </div>
+  );
 }
+
+export default App;
